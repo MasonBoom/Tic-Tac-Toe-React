8bf613b9ab2914ed7b8b14945d8b4cb8ad0cda55
MasonBoom <masonboom@gmail.com>
2022-12-28 04:03:22 -0500

    created modal, added new font

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -3,6 +3,8 @@ import {
     MainDiv,
     IconRow,
     ColoredIcon,
+    SelectionModal,
+    Header,
 } from './mainpage.styles';
 import x from '../../assets/x.svg';
 import o from '../../assets/o.svg';
@@ -14,6 +16,9 @@ const MainPage = () => {
                 <ColoredIcon src={x} alt='x' />
                 <ColoredIcon src={o} alt='o' />
             </IconRow>
+            <SelectionModal>
+                <Header>Pick Player 1's mark</Header>
+            </SelectionModal>
         </MainDiv>
     )
 }
--- src/pages/mainPage/mainpage.styles.js
+++ src/pages/mainPage/mainpage.styles.js
@@ -17,3 +17,25 @@ export const ColoredIcon = styled.img`
     width: 30px;
     height: 30px;
 `;
+
+export const SelectionModal = styled.div`
+    background-color: #1F3641;
+    box-shadow: inset 0px -8px 0px #10212A;
+    border-radius: 15px;
+    width: 100%;
+    height: 191px;
+    margin-top: 40px;
+    padding-top: 24px;
+`;
+
+export const Header = styled.h3`
+    color: #A8BFC9;
+    font-family: 'Outfit';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+`;
