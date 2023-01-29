265416656679a296e0f2f75a61bb9efcdbc5b421
MasonBoom <masonboom@gmail.com>
2023-01-28 19:40:06 -0500

    Player turn modal added

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
   GameContainer,
   TopRow,
+  PlayerTurn,
 } from './game.styles';
 import {
   IconRow,
@@ -9,6 +10,8 @@ import {
 } from '../mainPage/mainpage.styles';
 import x from '../../assets/x.svg';
 import o from '../../assets/o.svg';
+import grayX from '../../assets/grayX.svg';
+import grayO from '../../assets/grayO.svg';
 
 const Game = () => {
   return (
@@ -18,6 +21,10 @@ const Game = () => {
           <ColoredIcon src={x} alt='x' />
           <ColoredIcon src={o} alt='o' />
         </IconRow>
+        <PlayerTurn>
+          <img src={grayO} alt='grayX' />
+          <span>Turn</span>
+        </PlayerTurn>
       </TopRow>
     </GameContainer>
   );
--- src/pages/game/game.styles.js
+++ src/pages/game/game.styles.js
@@ -3,11 +3,42 @@ import styled from 'styled-components';
 export const GameContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 130px 550px;
+  margin: 130px 700px;
 `;
 
 export const TopRow = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+`;
+
+export const PlayerTurn = styled.div`
+  background: #1F3641;
+  box-shadow: inset 0px -4px 0px #10212A;
+  border-radius: 10px;
+  width: 140px;
+  height: 52px;
+  margin: auto;
+  margin-top: -10px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  span {
+    font-family: 'Outfit';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    color: #A8BFC9;
+
+  }
+
+  img {
+    margin-right: 10px;
+    margin-top: -2px;
+  }
 `;
