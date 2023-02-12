7c5c3249c2301ddd378ec7520039c03eba10006a
MasonBoom <masonboom@gmail.com>
2023-02-11 21:14:04 -0500

    changed route name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<NewGamePage />} />
-          <Route exact path="/start" element={<MainGameBoard />} />
+          <Route exact path="/game" element={<MainGameBoard />} />
         </Routes>
       </Router>
     </ContextProvider>
